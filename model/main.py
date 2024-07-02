bed70112ae41890e81c0440678a2ceaf2c637dbf
unknown <hafsaouajdimp@gmail.com>
2024-07-02 21:26:40 +0200

    add code

--- model/main.py
+++ model/main.py
@@ -1,9 +1,33 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score,classification_report
+import pickle
 
 def create_model(data):
     X,y=data.drop(['diagnosis'],axis=1),data['diagnosis']
-    
+    X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42)
+    scaler=StandardScaler()
+
+    scaler.fit_transform(X_train)
+
+    X_train=scaler.transform(X_train)
+    X_test=scaler.transform(X_test)
+
+    model=LogisticRegression()
+
+    model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+
+    print("Accuracy of the model: ",accuracy_score(y_test,y_pred))
+    print("Classification repport: ",classification_report(y_test,y_pred))
+
+    return model,scaler
+
+
 
 def get_clean_data():
     data=pd.read_csv("data/data.csv")
@@ -17,8 +41,13 @@ def get_clean_data():
     
 def main():
     data=get_clean_data()
-    model=create_model(data)
-    
-    
+    model,scaler=create_model(data)
+
+    with open('model/model.pkl','wb') as f:
+        pickle.dump(model,f)
+
+    with open('model/scaler.pkl','wb') as f:
+        pickle.dump(scaler,f)
+
 if __name__=='__main__':
     main()
